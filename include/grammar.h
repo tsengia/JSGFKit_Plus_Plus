6cfce28666d937e686bcfd80c3d04eb1882c9ce3
ExpandingDev <tylersengia@gmail.com>
2019-05-24 18:58:35 -0400

    Added pointer to istream constructor for Grammar

--- include/grammar.h
+++ include/grammar.h
@@ -71,7 +71,7 @@ class Grammar
         Grammar(std::string grammarName);
 
         Grammar(std::istream & inputStream);
-        Grammar(std::unique_ptr<std::istream> inputStream);
+        Grammar(std::istream * inputStream);
 
         /** Default destructor */
         ~Grammar();
