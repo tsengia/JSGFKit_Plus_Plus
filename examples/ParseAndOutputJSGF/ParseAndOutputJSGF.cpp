83b68c3130a68f135bf7b07e54e52be08f3cd111
John Doe <someone@somewhere.world>
2019-08-09 18:13:19 -0400

    Updated example program to optionally read and parse a grammar file from the command line.

--- examples/ParseAndOutputJSGF/ParseAndOutputJSGF.cpp
+++ examples/ParseAndOutputJSGF/ParseAndOutputJSGF.cpp
@@ -6,9 +6,11 @@ using namespace std;
 /**
   * \file ParseAndOutputJSGF.cpp
   * A simple program that shows how to parse a JSGF string to create a new Grammar object and then generate JSGF text using that Grammar object.
+  *	You can either use it with no command line options and it will use a built in sample grammar.
+  * It can also open up a file given via the command line, parse it, and echo it.
   */
 
-int main()
+int main(int argc, char *argv[])
 {
     // string that we will be parsing to create a JSGF Grammar object
     string s = "\
@@ -17,20 +19,23 @@ int main()
     <yolo> = [blarg | (rarg | hraaanngggg)];\
     <test> = hi {greeting} how are (you {you} | they {others}) today;\
     ";
-    //Parsing the string to create a Grammar object
-    Grammar g;
-    Grammar::parseGrammarFromString(s, g);
-
-    //Generate the JSGF definitiopn for the public command Rule
-    cout << "Generating <command> Rule:" << endl;
-    cout << g.getRule("command")->getRuleString() << endl << endl;;
+	Grammar g;
+	if(argc > 1) {
+		std::ifstream inputFile;
+		inputFile.open(argv[1]);
+		if(inputFile.good()) {
+			Grammar::parseGrammar(inputFile, g);
+		}
+	}
+	else {
+		//Parsing the string to create a Grammar object
+		Grammar::parseGrammarFromString(s, g);
+	}
 
     //Generate a text version of our Grammar object
     cout << "PARSED GRAMMAR:" << endl;
     cout << g.getText() << endl << endl;
 
     cout << "Test Done" << endl;
-    cout << "Press enter to exit" << endl;
-    getline(cin, s);
     return 0;
 }
