bcd47344dfa4187c1538146c138d2864ca66d45c
ExpandingDev <javscripter@yahoo.com>
2020-01-26 19:03:35 -0500

    Added unit tests

---
+++ test/BatchTest/test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "JSGF Kit++ Batch Grammar Tester"
+if [ ! -f parsetest ]; then
+	echo "ERROR: Please build and copy the ParseAndOutputJSGF example program into this directory!"
+	exit -2;
+fi
+
+for g in grammars/*/*; do
+	./parsetest $g &> grammar_test.log
+	if [ ! $? -eq 0 ]; then
+		echo "Test failed on grammar: $g ! Logged to grammar_test.log"
+		exit -1;
+	fi
+done
+
+echo "Tests passed!"
