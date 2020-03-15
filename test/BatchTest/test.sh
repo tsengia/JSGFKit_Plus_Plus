b4353336efc36c3f0cd09e5d7423beac1631270d
John Doe <someone@somewhere.world>
2020-03-14 20:59:36 -0400

    Added test for rules matching

--- test/BatchTest/test.sh
+++ test/BatchTest/test.sh
@@ -10,6 +10,17 @@ if [ ! -f parsetest ]; then
 	fi
 fi
 
+if [ ! -f jsgfmatch ]; then
+	if [ -f ../jsgfmatch ]; then
+		cp ../jsgfmatch jsgfmatch
+	else
+		echo "ERROR: Please build and copy the jsgfmatch example program into this directory!"
+		exit -2;
+	fi
+fi
+
+
+echo "Running parsing tests..."
 for g in grammars/*/*; do
 	./parsetest $g &> grammar_test.log
 	if [ ! $? -eq 0 ]; then
@@ -17,5 +28,16 @@ for g in grammars/*/*; do
 		exit -1;
 	fi
 done
+
+echo "Running matching tests..."
+echo "Testing rules..."
+while IFS=, read -r grammar inputTest expectedResult
+do
+	a=$( echo $inputTest | ./jsgfmatch -r -f $grammar )
+	if [ "$a" != "$expectedResult" ]; then
+		echo "For grammar $grammar , input of \"$inputTest\" produced \"$a\", but \"$expectedResult\" was expected!"
+		exit -1;
+	fi
+done < rules_test.csv
 
 echo "Tests passed!"
